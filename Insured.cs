d8abaab2b6fd23369196d63c5bc921d1f4b40439
aaroniusv <colormelt@gmail.com>
2016-01-05 09:40:06 -0500

    split up the classes

---
+++ Insured.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TropicsHomework01
+{
+    class Insured
+    {
+        public int initialPremium { get; set; }
+        public int numberOfQuotes { get; set; }
+        public double[] Percentages { get; set; }
+        public double[] Credits { get; set; }
+        public void getNumberOfQuotes()
+        {
+            string input;
+            Console.WriteLine("How many quotes would you like to process? ");
+            input = Console.ReadLine();
+            if (Regex.IsMatch(input, @"^[1-5]$"))
+            {
+                this.numberOfQuotes = Convert.ToInt32(input);
+            }
+            else
+            {
+                Console.WriteLine("Please enter a number between 1 and 5");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+        }
+        public void getPercentages()
+        {
+            this.Percentages = new double[3];
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Enter Percentage " + Convert.ToInt32(i + 1));
+                string input = Console.ReadLine();
+                if (Regex.IsMatch(input, @"^[1-9]|1[0-9]?|2[0-5]?$"))
+                {
+                    this.Percentages[i] = (Convert.ToDouble(input)*.01);
+                }
+                else if (Regex.IsMatch(input, @"^0[0]?$"))
+                {
+                    break;
+                }
+            }
+            this.Credits = new double[3];
+            this.Credits[0] = this.initialPremium * this.Percentages[0];
+            double prem = this.initialPremium - this.Credits[0];
+            //Console.WriteLine(this.Credits[0]);
+            for (int i = 1; i < 3; i++)
+            {
+                this.Credits[i] = prem * this.Percentages[i];
+                prem -= this.Credits[i];
+
+
+            }
+
+        }
+      
+
+
+        public void getInitialPremium()
+        {
+            
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("Enter the insurance premium: ");
+            string input;
+            int value;
+            input = Console.ReadLine();
+            if(Int32.TryParse(input, out value))
+            {
+                if(Convert.ToInt32(input) >= 0 && Convert.ToInt32(input) <=100000)
+                {
+                    this.initialPremium = Convert.ToInt32(input);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Enter a numeric value between 0 and 100000.");
+                Environment.Exit(0);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TropicsHomework01
+{
+    class Program
+    {
+        
+
+
+        static void Main(string[] args)
+        {
+            var helper = new Insured();
+            helper.getNumberOfQuotes();
+            var q = new List<Insured>();
+            for (int i = 0; i < helper.numberOfQuotes; i++)
+            {
+                q.Add(new Insured());
+                
+            }
+            for (int i = 0; i < helper.numberOfQuotes; i++)
+            {
+                Console.WriteLine("\nPolicy " + Convert.ToInt32(i+1));
+                q[i].getInitialPremium();
+                q[i].getPercentages();
+            }
+            for (int i = 0; i < helper.numberOfQuotes; i++)
+            {
+                Console.WriteLine("Policy " + Convert.ToInt16(i+1));
+                Console.WriteLine("--------------------------------");
+                Console.WriteLine("Initial Premium:\t"+ q[i].initialPremium.ToString("C"));
+
+                for (int p = 0; p < 3; p++)
+                {
+                    Console.WriteLine("Credit " + Convert.ToInt32(p + 1) + " ("+ q[i].Percentages[p] *100  +"%):\t"+ q[i].Credits[p].ToString("C") );
+                    //Console.WriteLine("Percentage"+ Convert.ToInt16(p+1) + ": \t"  +  q[i].Percentages[p]);
+                }
+            }
+            Console.ReadLine();
+
+         }
+    }
+}
